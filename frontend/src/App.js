2a2a4e7af6d9f23e43d814529e5270deaa097d85
yujinkim <yujinkim@valuemark.co.kr>
2025-03-21 16:52:19 +0900

    로그인페이지 수정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,14 +6,15 @@ function App() {
   return (
     <div className="LoginPg">
       <header className="App-header">
-        <img src="{logoImg}" alt="creative-lab" style={{width: '100px'}}/>
+      <img src={logoImg} alt="creative-lab" style={{ width: "100px" }} />
         <input type="text" className="input-style" placeholder="아이디를 입력해 주세요."/>
         <input type="password" className="input-style" placeholder="비밀번호를 입력해 주세요."/>
         <input type="checkbox" id="saveId"/><label for="saveId"><span></span>아이디 저장</label>
         <button className="login-button" type="button">로그인</button>
-        <a href="">아이디/비밀번호 찾기</a>
+        <a href="#">아이디/비밀번호 찾기</a>
       </header>
     </div>
+  )
 }
 
 export default App;

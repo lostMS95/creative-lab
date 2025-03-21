267df4ffa7b922ce124193c62c4833c9d4c3796c
yujinkim <yujinkim@valuemark.co.kr>
2025-03-21 16:37:36 +0900

    로그인페이지 수정 및 파일 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import HomePage from './pages/home/HomePage';
-
+import logoImg from './img_upload.png';
+import './Reset.css';
+import './App.css';
 
 function App() {
   return (
-      <Router>
-        <div>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-          </Routes>
-        </div>
-      </Router>
-  );
+    <div className="LoginPg">
+      <header className="App-header">
+        <img src="{logoImg}" alt="creative-lab" style={{width: '100px'}}/>
+        <input type="text" className="input-style" placeholder="아이디를 입력해 주세요."/>
+        <input type="password" className="input-style" placeholder="비밀번호를 입력해 주세요."/>
+        <input type="checkbox" id="saveId"/><label for="saveId"><span></span>아이디 저장</label>
+        <button className="login-button" type="button">로그인</button>
+        <a href="">아이디/비밀번호 찾기</a>
+      </header>
+    </div>
 }
 
 export default App;
